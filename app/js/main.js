03e1555555c7c72228300e9ea975a9bc9bf971ea
Alek21 <aleksandarbatin@gmail.com>
2016-07-28 19:47:09 +0200

    dropdown menu

--- app/js/main.js
+++ app/js/main.js
@@ -14,13 +14,6 @@ $(document).ready(function(){
   	cssEase: 'linear'
   });
 
-  $('.dropdown-menu').mouseover(function(){
-    $(this.parentNode.firstChild).css({'background-color': '#76D6F9','color':'#fff'});
-
-  });
-   $('.dropdown-menu').mouseout(function(){
-    $(this.parentNode.firstChild).css({'background-color': 'black','color':'#86b4d9'});
-  });
 });
 
 
