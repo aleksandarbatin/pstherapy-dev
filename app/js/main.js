c2c75f8aa47ec4ff8f3078cc039e830c59a15364
Aleksandar Batin <aleksandar.batin@diwanee.com>
2016-11-18 12:42:18 +0100

    custom slider

--- app/js/main.js
+++ app/js/main.js
@@ -3,18 +3,23 @@
 //**************
 
 $(document).ready(function(){
-  $('.slider_holder').slick({
-  	dots: true,
-  	slidesToShow: 1,
-  	slidesToScroll: 1,
-  	autoplay: true,
-  	infinite: true,
-  	speed: 500,
-  	fade: true,
-    arrows: false,
-  	cssEase: 'linear'
+
+  var $slides = $(".slide-block"),
+      $slides_container = $(".slider_container");
+
+  $slides_container.css({"width":100*$slides.length+'%'});
+        $slides.css({"width":100/$slides.length+'%'});
+        setInterval(function(){
+        var first = $slides[0];
+        $slides_container.animate({"left":'-100%'}, 1000 ,  function(){
+            $( first).remove();
+            $slides_container.append( first);
+            $slides = $(".slide-block");
+           $slides_container.css({"left":"0%"});
+       });
+
+    },4000);
   });
-});
 
   // #####################################
   // Toggle mobile menu
